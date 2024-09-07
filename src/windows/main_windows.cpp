53118829ad633b42b1eeca35cb1e1f0926368f47
YMsyaraf <YMsyaraf@hotmail.com>
2024-09-07 22:55:45 +0800

    Adding -h --version command

--- src/windows/main_windows.cpp
+++ src/windows/main_windows.cpp
@@ -1,7 +1,32 @@
 #include "../common/NanoDir.h"
-#include "resources.h"
 
-int main() {
+void showHelp() {
+    std::cout << "NanoDir - A terminal-based file manager\n";
+    std::cout << "Usage: nanodir [options] [directory]\n";
+    std::cout << "Options:\n";
+    std::cout << "  -h, --help       Show this help message\n";
+    std::cout << "  --version        Show version information\n";
+}
+
+void showVersion() {
+    std::cout << "NanoDir version "
+              << NANODIR_VERSION_MAJOR << "."
+              << NANODIR_VERSION_MINOR << "."
+              << NANODIR_VERSION_PATCH << "\n";
+}
+
+int main(int argc, char *argv[]) {
+    if (argc > 1) {
+        std::string arg = argv[1];
+        if (arg == "-h" || arg == "--help") {
+            showHelp();
+            return 0;
+        } else if (arg == "--version") {
+            showVersion();
+            return 0;
+        }
+    }
+
     NanoDir app;
     app.run();
     return 0;
